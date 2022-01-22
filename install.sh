9d3150a08aab5cd6b704097db23d2a522437270f
IHA-arch <80745543+IHA-arch@users.noreply.github.com>
2022-01-22 19:41:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 mkdir /usr/share/DNSInfo_IHA
-touch /usr/share/DNSInfo_IHA/APIkey
-cp dnsinfo /usr/local/bin
+mv APIkey /usr/share/DNSInfo_IHA
+mv dnsinfo /usr/local/bin
 chmod +x /usr/local/bin/dnsinfo
-cp DNSInfo.py /usr/share/DNSInfo_IHA
+mv DNSInfo.py /usr/share/DNSInfo_IHA
 echo "type 'dnsinfo' and use"
-
-
